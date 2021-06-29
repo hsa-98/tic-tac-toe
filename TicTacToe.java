104ca0b3c10b5f53c00a3130d90ca48b7235070f
hsa-98 <harshsanjayagrawal89@gmail.com>
2021-06-29 17:22:50 +0530

    Allows a player to choose X or O

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,13 +1,33 @@
+import java.util.Scanner;
+
 public class TicTacToe {
     char[]table =  new char[10];
+    Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         TicTacToe ticTacToe = new TicTacToe();
         ticTacToe.CreateEmptyTable();
+        char playerChar = ticTacToe.playerSymbol();
+
     }
     public void CreateEmptyTable(){
         for(int i = 0;i<table.length;i++){
             table[i]=0;
         }
     }
-    
+    public char playerSymbol(){
+        System.out.println("Enter X to choose x or enter the letter O to chose o");
+        char choice = sc.next().charAt(0);
+        if(choice=='x'||choice=='X'){
+            System.out.println("X is chosen by the user");
+            System.out.println("O is given to the computer");
+        }
+        else if(choice=='O'||choice=='o'){
+            System.out.println("O is chosen by the user");
+            System.out.println("X is given to the computer");
+        }
+        else{
+            System.out.println("Enter a valid input");
+        }
+        return choice;
+    }
 }
