66c031d029026aa0b6ec1c7d4b3011e354650b95
hsa-98 <harshsanjayagrawal89@gmail.com>
2021-06-29 17:53:37 +0530

    Can display board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,33 +1,42 @@
 import java.util.Scanner;
 
 public class TicTacToe {
-    char[]table =  new char[10];
+    char[] table = new char[10];
     Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         TicTacToe ticTacToe = new TicTacToe();
         ticTacToe.CreateEmptyTable();
         char playerChar = ticTacToe.playerSymbol();
-
+        ticTacToe.displayBoard();
     }
-    public void CreateEmptyTable(){
-        for(int i = 0;i<table.length;i++){
-            table[i]=0;
+
+    public void CreateEmptyTable() {
+        for (int i = 0; i < table.length; i++) {
+            table[i] = 0;
         }
     }
-    public char playerSymbol(){
+
+    public char playerSymbol() {
         System.out.println("Enter X to choose x or enter the letter O to chose o");
         char choice = sc.next().charAt(0);
-        if(choice=='x'||choice=='X'){
+        if (choice == 'x' || choice == 'X') {
             System.out.println("X is chosen by the user");
             System.out.println("O is given to the computer");
-        }
-        else if(choice=='O'||choice=='o'){
+        } else if (choice == 'O' || choice == 'o') {
             System.out.println("O is chosen by the user");
             System.out.println("X is given to the computer");
-        }
-        else{
+        } else {
             System.out.println("Enter a valid input");
         }
         return choice;
     }
+
+    public void displayBoard() {
+        for (int i = 1; i < table.length; i = i + 3) {
+            System.out.print(table[i] + " " + table[i + 1] + " " + table[i + 2]);
+            System.out.println();
+        }
+    }
+    
 }
